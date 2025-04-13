a4bb29eabe7579a58653ff52f6a97bc301e380ce
Kowsar Hossain <hr.rahman678@gmail.com>
2025-04-13 16:55:35 +0600

    Project Working

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -4,13 +4,21 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Project;  
+use Yajra\DataTables\Facades\DataTables;
 
 class ProjectController extends Controller
 {
     public function index(Request $request){
-        if($request->ajax(){
-            dd:($request->all());
-        })
+        if($request->ajax()){
+            $projects = Project::query();
+ 
+            return DataTables::eloquent($projects)
+            ->addColumn('created_at', function($projects){
+                return ($projects->created_at)->format('D, M Y');
+            })
+            ->make(true);
+        }
         return view('backend.projects.index');
     }
 }
